b9645389a8060935d5c134bd5d4fd61b1a468e5e
Miguel Angel <pablotoctom@gmail.com>
2017-01-21 20:43:37 -0500

    Instruccion Condicional
    Instruccion Condicional de Java-Switch

---
+++ src/labo/EjemploSwitch.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author miguel
+ */
+public class EjemploSwitch {
+
+    public static void main(String[] args) {
+        int x;
+        do {
+            //ingreso de entero como cadena
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Ingrese Entero:");
+            String num = scanner.nextLine();
+            //scanner.close();
+            //convertir
+            
+             x=Integer.valueOf(num).intValue();
+
+            switch (x) {
+
+                case 0:
+                    System.out.println("domingo");
+                    break;
+                case 1:
+                    System.out.println("lunes");
+                    break;
+                case 2:
+                    System.out.println("martes");
+                    break;
+                case 3:
+                    System.out.println("miercoles");
+                    break;
+                default:
+                    System.out.println("Valor incorrecto!!");
+            }
+
+        } while (x != 0 && x!=1 && x!=2 && x!=3);
+         System.out.println("***Ha ingresado un valor correcto!! - GRacias***");
+        //haciendo comparaciones
+        /*
+        switch (x) {
+
+            case 0:
+                System.out.println("domingo");
+                break;
+            case 1:
+                System.out.println("lunes");
+                break;
+            case 2:
+                System.out.println("martes");
+                break;
+            case 3:
+                System.out.println("miercoles");
+                break;
+            default:
+                System.out.println("Valor incorrecto");
+        }
+         */
+      
+        
+    }
+}
